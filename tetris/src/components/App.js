4096be5b429318f52f12ccae0232897a93ea1992
michael henrys <michael42h@gmail.com>
2022-07-23 14:29:20 +1200

    Add GameArea and Board

---
+++ tetris/src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+import GameArea from './GameArea';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Tetris</p>
+      </header>
+      <GameArea />
+    </div>
+  );
+}
+
+export default App;
---
+++ tetris/src/components/GameArea.js
@@ -0,0 +1,10 @@
+import '../styles/GameArea.css'
+import Board from './Board.js';
+
+export default function GameArea() {
+  return (
+    <div className='GameArea'>
+      <Board/>
+    </div>
+  )
+}
